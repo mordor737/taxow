d7137f6e0dd0fa113108974e638ee0711dae03f2
mordor737 <procomtech737@gmail.com>
2021-07-27 13:08:06 +0530

    minor change related to Form

--- src/app/add-update-taxrates/add-update-taxrates.component.ts
+++ src/app/add-update-taxrates/add-update-taxrates.component.ts
@@ -1,22 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl,Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-update-taxrates',
   templateUrl: './add-update-taxrates.component.html',
-  styleUrls: ['./add-update-taxrates.component.css']
+  styleUrls: ['./add-update-taxrates.component.css'],
 })
 export class AddUpdateTaxratesComponent implements OnInit {
+  addUpdateForm: FormGroup;
 
-  itemFormControl = new FormControl('', [
-    Validators.required,
-  ]);
+  buttonType: String = 'Add';
 
-  buttonType:String="Add";
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.addUpdateForm = this.formBuilder.group({
+      item: ['', [Validators.required]],
+      taxRate: ['', [Validators.required]],
+    });
   }
 
+  ngOnInit(): void {}
+
+  get formControls() {
+    return this.addUpdateForm.controls;
+  }
+  onSubmit() {
+    console.log(this.addUpdateForm.value);
+  }
 }
--- src/app/enter-record/enter-record.component.ts
+++ src/app/enter-record/enter-record.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl,Validators } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 
 interface Item {
   value: string;
@@ -15,54 +15,51 @@ interface ItemGroup {
 @Component({
   selector: 'app-enter-record',
   templateUrl: './enter-record.component.html',
-  styleUrls: ['./enter-record.component.css']
+  styleUrls: ['./enter-record.component.css'],
 })
 export class EnterRecordComponent implements OnInit {
-  quantityFormControl = new FormControl('', [
-    Validators.required,
-  ]);
+  quantity = new FormControl('', [Validators.required]);
 
-  itemControl = new FormControl();
+  item = new FormControl();
   itemGroups: ItemGroup[] = [
     {
       name: 'Groceries',
       item: [
-        {value: 'Sugar', viewValue: 'Sugar'},
-        {value: 'Tea', viewValue: 'Tea'},
-        {value: 'Milk', viewValue: 'Milk'}
-      ]
+        { value: 'Sugar', viewValue: 'Sugar' },
+        { value: 'Tea', viewValue: 'Tea' },
+        { value: 'Milk', viewValue: 'Milk' },
+      ],
     },
     {
       name: 'Water',
       item: [
-        {value: 'squirtle-3', viewValue: 'Squirtle'},
-        {value: 'psyduck-4', viewValue: 'Psyduck'},
-        {value: 'horsea-5', viewValue: 'Horsea'}
-      ]
+        { value: 'squirtle-3', viewValue: 'Squirtle' },
+        { value: 'psyduck-4', viewValue: 'Psyduck' },
+        { value: 'horsea-5', viewValue: 'Horsea' },
+      ],
     },
     {
       name: 'Fire',
       disabled: true,
       item: [
-        {value: 'charmander-6', viewValue: 'Charmander'},
-        {value: 'vulpix-7', viewValue: 'Vulpix'},
-        {value: 'flareon-8', viewValue: 'Flareon'}
-      ]
+        { value: 'charmander-6', viewValue: 'Charmander' },
+        { value: 'vulpix-7', viewValue: 'Vulpix' },
+        { value: 'flareon-8', viewValue: 'Flareon' },
+      ],
     },
     {
       name: 'Psychic',
       item: [
-        {value: 'mew-9', viewValue: 'Mew'},
-        {value: 'mewtwo-10', viewValue: 'Mewtwo'},
-      ]
-    }
+        { value: 'mew-9', viewValue: 'Mew' },
+        { value: 'mewtwo-10', viewValue: 'Mewtwo' },
+      ],
+    },
   ];
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onSubmit(itemForm:any){
+  onSubmit(itemForm: any) {
     console.log(itemForm.value);
   }
 }
